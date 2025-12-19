590d276e20e1cd1fad8a4ad06866ae2a074d4ac7
validk <104666454+validk@users.noreply.github.com>
2025-12-19 16:39:28 +0530

    logging enable and working

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.hello.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+    private static final Logger log =
+            LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
-        return "HELLO WORKS";
+        log.info("Hello endpoint invoked");
+        log.debug("Debug trace: controller execution successful");
+        return "Hello from Spring MVC on WebLogic";
     }
 }
